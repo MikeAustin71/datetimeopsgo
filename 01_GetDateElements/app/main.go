9002e0070614b46ea70b36dff8d7d262fadbc836
Mike Rapp <mike@paladinacs.net>
2017-05-10 23:03:28 -0500

    Added Elapsed Time Calculations

--- 01_GetDateElements/app/main.go
+++ 01_GetDateElements/app/main.go
@@ -6,52 +6,27 @@ import (
 	"time"
 )
 
+/*
+import (
+	common "MikeAustin71/datetimeopsgo/01_GetDateElements/common"
+	"fmt"
+	"time"
+)
+*/
+
 func main() {
-
-	dt3()
-
-}
-
-/*
-func dt1()  {
-// Get current time in the form of a string
-	tstr := "04/29/2017 19:54:30 -0500 CDT"
-	fmtstr := "01/02/2006 15:04:05 -0700 MST"
-
-	t, err := time.Parse(fmtstr, tstr)
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	result := common.GetDateTimeStr(t)
-
-	fmt.Println(tstr, "=", t)
-	fmt.Println("result=", result)
-
-}
-*/
-
-/*
-func dt2() {
-
-	dt := common.DateTimeUtility{}
-	now := time.Now()
-
-	s := dt.GetDateTimeEverything(now)
-
-	fmt.Println("Time String: ", s)
-
-}
-*/
-
-func dt3() {
-	tstr := "04/29/2017 19:54:30.123456489 -0500 CDT"
+	tstr1 := "04/15/2017 19:54:30.123456489 -0500 CDT"
+	tstr2 := "04/18/2017 09:21:16.987654321 -0500 CDT"
 	fmtstr := "01/02/2006 15:04:05.000000000 -0700 MST"
-	testTime, _ := time.Parse(fmtstr, tstr)
+	t1, _ := time.Parse(fmtstr, tstr1)
+	t2, _ := time.Parse(fmtstr, tstr2)
 	dt := common.DateTimeUtility{}
-	str := dt.GetDateTimeEverything(testTime)
-	fmt.Println("Everything Format: ", str)
-	// Saturday April 29, 2017 19:54:30.123456489 -0500 CDT
+	dur, _ := dt.GetDuration(t1, t2)
+	ed, _ := dt.GetElapsedTime(t1, t2)
+	fmt.Println("Elapsed Time: ", ed.DurationStr)
+	// "2 Days 13 Hours 26 Minutes 46 Seconds 864 Milliseconds 197 Microseconds 832 Nanoseconds"
+
+	fmt.Println("")
+	fmt.Println("Default Duration: ", dur)
+	// 61h26m46.864197832s
 }
--- 01_GetDateElements/common/getintdatetimevalues.go
+++ 01_GetDateElements/common/getintdatetimevalues.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -25,4 +26,55 @@ func GetCurrentTimeAsInts() {
 	fmt.Println("The integer second is:", i)
 	i = int64(t.Nanosecond())
 	fmt.Println("The integer nanosecond is", i)
+}
+
+// GetEverythingFormat - provides a sample of the
+// 'GetEverything Date Time Format!
+func GetEverythingFormat() {
+	tstr := "04/29/2017 19:54:30.123456489 -0500 CDT"
+	fmtstr := "01/02/2006 15:04:05.000000000 -0700 MST"
+	testTime, _ := time.Parse(fmtstr, tstr)
+	dt := DateTimeUtility{}
+	str := dt.GetDateTimeEverything(testTime)
+	fmt.Println("Everything Format: ", str)
+	// Saturday April 29, 2017 19:54:30.123456489 -0500 CDT
+}
+
+// GetCurrentTimeAsString - Get current time in the form of a string
+func GetCurrentTimeAsString() {
+	tstr := "04/29/2017 19:54:30 -0500 CDT"
+	fmtstr := "01/02/2006 15:04:05 -0700 MST"
+	dt := DateTimeUtility{}
+	t, err := time.Parse(fmtstr, tstr)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	result := dt.GetDateTimeStr(t)
+
+	fmt.Println(tstr, "=", t)
+	fmt.Println("result=", result)
+}
+
+// GetBasicDuration - Returns basic duration as a string
+func GetBasicDuration() {
+	t1str := "04/29/2017 19:54:30 -0500 CDT"
+	t2str := "04/29/2017 20:56:32 -0500 CDT"
+	fmtstr := "01/02/2006 15:04:05 -0700 MST"
+	dt := DateTimeUtility{}
+	t1, err := time.Parse(fmtstr, t1str)
+	if err != nil {
+		panic(errors.New("Time Parse1 Error:" + err.Error()))
+	}
+
+	t2, err := time.Parse(fmtstr, t2str)
+	if err != nil {
+		panic(errors.New("Time Parse2 Error:" + err.Error()))
+	}
+
+	duration, err := dt.GetDuration(t1, t2)
+
+	fmt.Println("Duration:", duration)
 }
