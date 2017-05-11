25c8907b4da89e3646d3c5b94fd511f0ac8e2fb4
Mike Rapp <mike@paladinacs.net>
2017-05-11 18:21:16 -0500

    Added TimeZoneUtility Directory

--- 02_DateTimeUtility/common/dattimeexamples.go
+++ 02_DateTimeUtility/common/dattimeexamples.go
@@ -78,3 +78,27 @@ func GetBasicDuration() {
 
 	fmt.Println("Duration:", duration)
 }
+
+// GetElapsedTimeDuration - example of
+// GetElapsedTime() in DateTime Utility
+func GetElapsedTimeDuration() {
+	tstr1 := "04/15/2017 19:54:30.123456489 -0500 CDT"
+	tstr2 := "04/18/2017 09:21:16.987654321 -0500 CDT"
+	fmtstr := "01/02/2006 15:04:05.000000000 -0700 MST"
+	t1, _ := time.Parse(fmtstr, tstr1)
+	t2, _ := time.Parse(fmtstr, tstr2)
+	dt := DateTimeUtility{}
+
+	ed, _ := dt.GetElapsedTime(t1, t2)
+	fmt.Println("Elapsed Time: ", ed.DurationStr)
+	// "2-Days 13-Hours 26-Minutes 46-Seconds 864-Milliseconds 197-Microseconds 832-Nanoseconds"
+
+	fmt.Println("")
+	fmt.Println("Default Duration: ", ed.DefaultStr)
+	// 61h26m46.864197832s
+
+	fmt.Println("")
+	fmt.Println("NanosecStr: ", ed.NanosecStr)
+	// 2-Days 13-Hours 26-Minutes 46-Seconds 864197832-Nanoseconds
+
+}
