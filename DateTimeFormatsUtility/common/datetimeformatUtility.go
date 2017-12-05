1e649d5334591a19ca8daf1822b9ab260fd9cf86
Mike Rapp <mike@paladinacs.net>
2017-12-04 18:01:05 -0600

    Code cleanup.

--- DateTimeFormatsUtility/common/datetimeformatUtility.go
+++ DateTimeFormatsUtility/common/datetimeformatUtility.go
@@ -17,7 +17,6 @@ import (
 	datetimeformatUtility.go is part of the date time operations library. The source code repository
  	for this file is located at:
 					https://github.com/MikeAustin71/datetimeopsgo.git
-
 */
 
 type DateTimeWriteFormatsToFileDto struct {
