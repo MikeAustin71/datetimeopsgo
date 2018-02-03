c10fb3b9f2e945e0ddb5c1315af053267d761993
Mike Rapp <mike@paladinacs.net>
2018-02-02 23:31:26 -0600

    Testing File time stamp formats

--- TimeZoneUtility/app/main.go
+++ TimeZoneUtility/app/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"MikeAustin71/datetimeopsgo/TimeZoneUtility/common"
 
+	"time"
+	"fmt"
 )
 
 /*
@@ -16,8 +18,14 @@ import (
 
 func main() {
 
-	common.Tex021()
+	appStartTimeTzu, _ := common.TimeZoneUtility{}.ConvertTz(time.Now().UTC(), "Local")
 
-
+	dt := common.DateTimeUtility{}
+	dateTimeStamp := dt.GetDateTimeStr(appStartTimeTzu.TimeOut)
+	logFileNameExt :=  "BaseFileName" + "_" + dateTimeStamp + ".log"
+	fmt.Println(" File Name Composite: ", logFileNameExt)
+	fmt.Println("     Date Time Stamp: ", dateTimeStamp)
+	dateTimeStamp = dt.GetDateTimeNanoSecText(appStartTimeTzu.TimeOut)
+	fmt.Println("Full Date Time Stamp: ", dateTimeStamp)
 
 }
