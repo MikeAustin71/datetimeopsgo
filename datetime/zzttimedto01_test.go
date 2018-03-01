58af2f39d5bc9038f4553adc156149627fa753f4
Mike Rapp <mike@paladinacs.net>
2018-02-28 21:59:39 -0600

    Added TimeDto test TestTimeDto_Empty

--- datetime/zzttimedto01_test.go
+++ datetime/zzttimedto01_test.go
@@ -56,7 +56,7 @@ func TestTimeDto_CopyOut_01(t *testing.T) {
 	}
 
 	if 300 != tDto.Nanoseconds {
-		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 539, tDto.Nanoseconds)
+		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 300, tDto.Nanoseconds)
 	}
 
 	if 515539300 != tDto.TotNanoseconds {
@@ -123,13 +123,12 @@ func TestTimeDto_CopyIn_01(t *testing.T) {
 	}
 
 	if 300 != tDto.Nanoseconds {
-		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 539, tDto.Nanoseconds)
+		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 300, tDto.Nanoseconds)
 	}
 
 	if 515539300 != tDto.TotNanoseconds {
 		t.Errorf("Error: Expected Total Nanoseconds='%v'.  Instead, Total Nanoseconds='%v'", 515539300, tDto.TotNanoseconds)
 	}
-
 
 }
 
@@ -184,13 +183,12 @@ func TestTimeDto_ConvertToNegativeValues(t *testing.T) {
 	}
 
 	if -300 != tDto.Nanoseconds {
-		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", -539, tDto.Nanoseconds)
+		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", -300, tDto.Nanoseconds)
 	}
 
 	if -515539300 != tDto.TotNanoseconds {
 		t.Errorf("Error: Expected Total Nanoseconds='%v'.  Instead, Total Nanoseconds='%v'", -515539300, tDto.TotNanoseconds)
 	}
-
 
 }
 
@@ -244,7 +242,7 @@ func TestTimeDto_ConvertToAbsoluteValues_01(t *testing.T) {
 	}
 
 	if -300 != tDto.Nanoseconds {
-		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", -539, tDto.Nanoseconds)
+		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", -300, tDto.Nanoseconds)
 	}
 
 	if -515539300 != tDto.TotNanoseconds {
@@ -294,12 +292,126 @@ func TestTimeDto_ConvertToAbsoluteValues_01(t *testing.T) {
 	}
 
 	if 300 != tDto.Nanoseconds {
-		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 539, tDto.Nanoseconds)
+		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 300, tDto.Nanoseconds)
 	}
 
 	if 515539300 != tDto.TotNanoseconds {
 		t.Errorf("Error: Expected Total Nanoseconds='%v'.  Instead, Total Nanoseconds='%v'", 515539300, tDto.TotNanoseconds)
 	}
+
+}
+
+func TestTimeDto_Empty(t *testing.T) {
+
+	tDto, err := TimeDto{}.New(2017, 4, 0, 30, 22, 58,32,0,0,515539300)
+
+	if err != nil {
+		t.Errorf("Error returned by TimeDto{}.New(2017, 4, 0, 30, 22, 58,32,0,0,515539300). Error='%v'", err.Error())
+	}
+
+	if 2017 != tDto.Years {
+		t.Errorf("Error: Expected Years='%v'.  Instead, Years='%v'", 2017, tDto.Years)
+	}
+
+	if 4 != tDto.Months {
+		t.Errorf("Error: Expected Months='%v'.  Instead, Months='%v'", 4, tDto.Months)
+	}
+
+	if 4 != tDto.Weeks {
+		t.Errorf("Error: Expected Weeks='%v'.  Instead, Weeks='%v'", 4, tDto.Weeks)
+	}
+
+	if 2 != tDto.WeekDays {
+		t.Errorf("Error: Expected WeekDays='%v'.  Instead, WeekDays='%v'", 2, tDto.WeekDays)
+	}
+
+	if 30 != tDto.DateDays {
+		t.Errorf("Error: Expected Date Days='%v'.  Instead, Date Days='%v'", 30, tDto.DateDays)
+	}
+
+	if 22 != tDto.Hours {
+		t.Errorf("Error: Expected Hours='%v'.  Instead, Hours='%v'", 22, tDto.Hours)
+	}
+
+	if 58 != tDto.Minutes {
+		t.Errorf("Error: Expected Minutes='%v'.  Instead, Minutes='%v'", 58, tDto.Minutes)
+	}
+
+	if 32 != tDto.Seconds {
+		t.Errorf("Error: Expected Seconds='%v'.  Instead, Seconds='%v'", 32, tDto.Seconds)
+	}
+
+	if 515 != tDto.Milliseconds {
+		t.Errorf("Error: Expected Milliseconds='%v'.  Instead, Milliseconds='%v'", 512, tDto.Milliseconds)
+	}
+
+	if 539 != tDto.Microseconds {
+		t.Errorf("Error: Expected Microseconds='%v'.  Instead, Microseconds='%v'", 539, tDto.Microseconds)
+	}
+
+	if 300 != tDto.Nanoseconds {
+		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 300, tDto.Nanoseconds)
+	}
+
+	if 515539300 != tDto.TotNanoseconds {
+		t.Errorf("Error: Expected Total Nanoseconds='%v'.  Instead, Total Nanoseconds='%v'", 515539300, tDto.TotNanoseconds)
+	}
+
+
+	tDto.Empty()
+
+	if err != nil {
+		t.Errorf("Error returned by TimeDto{}.New(2017, 4, 0, 30, 22, 58,32,0,0,515539300). Error='%v'", err.Error())
+	}
+
+	if 0 != tDto.Years {
+		t.Errorf("Error: Expected Years='%v'.  Instead, Years='%v'", 0, tDto.Years)
+	}
+
+	if 0 != tDto.Months {
+		t.Errorf("Error: Expected Months='%v'.  Instead, Months='%v'", 0, tDto.Months)
+	}
+
+	if 0 != tDto.Weeks {
+		t.Errorf("Error: Expected Weeks='%v'.  Instead, Weeks='%v'", 0, tDto.Weeks)
+	}
+
+	if 0 != tDto.WeekDays {
+		t.Errorf("Error: Expected WeekDays='%v'.  Instead, WeekDays='%v'", 0, tDto.WeekDays)
+	}
+
+	if 0 != tDto.DateDays {
+		t.Errorf("Error: Expected Date Days='%v'.  Instead, Date Days='%v'", 0, tDto.DateDays)
+	}
+
+	if 0 != tDto.Hours {
+		t.Errorf("Error: Expected Hours='%v'.  Instead, Hours='%v'", 0, tDto.Hours)
+	}
+
+	if 0 != tDto.Minutes {
+		t.Errorf("Error: Expected Minutes='%v'.  Instead, Minutes='%v'", 0, tDto.Minutes)
+	}
+
+	if 0 != tDto.Seconds {
+		t.Errorf("Error: Expected Seconds='%v'.  Instead, Seconds='%v'", 0, tDto.Seconds)
+	}
+
+	if 0 != tDto.Milliseconds {
+		t.Errorf("Error: Expected Milliseconds='%v'.  Instead, Milliseconds='%v'", 0, tDto.Milliseconds)
+	}
+
+	if 0 != tDto.Microseconds {
+		t.Errorf("Error: Expected Microseconds='%v'.  Instead, Microseconds='%v'", 0, tDto.Microseconds)
+	}
+
+	if 0 != tDto.Nanoseconds {
+		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 0, tDto.Nanoseconds)
+	}
+
+	if 0 != tDto.TotNanoseconds {
+		t.Errorf("Error: Expected Total Nanoseconds='%v'.  Instead, Total Nanoseconds='%v'", 0, tDto.TotNanoseconds)
+	}
+
 
 }
 
@@ -371,7 +483,7 @@ Total Nanoseconds:  515539300
 	}
 
 	if 300 != tDto.Nanoseconds {
-		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 539, tDto.Nanoseconds)
+		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 300, tDto.Nanoseconds)
 	}
 
 	if 515539300 != tDto.TotNanoseconds {
