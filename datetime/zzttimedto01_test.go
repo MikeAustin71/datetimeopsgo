626c4e86acca1e1f860d381e2f6f0e6a7f5e3d78
Mike Rapp <mike@paladinacs.net>
2018-02-28 22:05:26 -0600

    Added TimeDto equality tests

--- datetime/zzttimedto01_test.go
+++ datetime/zzttimedto01_test.go
@@ -412,8 +412,137 @@ func TestTimeDto_Empty(t *testing.T) {
 		t.Errorf("Error: Expected Total Nanoseconds='%v'.  Instead, Total Nanoseconds='%v'", 0, tDto.TotNanoseconds)
 	}
 
-
 }
+
+func TestTimeDto_Equal_01(t *testing.T) {
+
+	tDto, err := TimeDto{}.New(2017, 4, 0, 30, 22, 58,32,0,0,515539300)
+
+	if err != nil {
+		t.Errorf("Error returned by TimeDto{}.New(2017, 4, 0, 30, 22, 58,32,0,0,515539300). Error='%v'", err.Error())
+	}
+
+	if 2017 != tDto.Years {
+		t.Errorf("Error: Expected Years='%v'.  Instead, Years='%v'", 2017, tDto.Years)
+	}
+
+	if 4 != tDto.Months {
+		t.Errorf("Error: Expected Months='%v'.  Instead, Months='%v'", 4, tDto.Months)
+	}
+
+	if 4 != tDto.Weeks {
+		t.Errorf("Error: Expected Weeks='%v'.  Instead, Weeks='%v'", 4, tDto.Weeks)
+	}
+
+	if 2 != tDto.WeekDays {
+		t.Errorf("Error: Expected WeekDays='%v'.  Instead, WeekDays='%v'", 2, tDto.WeekDays)
+	}
+
+	if 30 != tDto.DateDays {
+		t.Errorf("Error: Expected Date Days='%v'.  Instead, Date Days='%v'", 30, tDto.DateDays)
+	}
+
+	if 22 != tDto.Hours {
+		t.Errorf("Error: Expected Hours='%v'.  Instead, Hours='%v'", 22, tDto.Hours)
+	}
+
+	if 58 != tDto.Minutes {
+		t.Errorf("Error: Expected Minutes='%v'.  Instead, Minutes='%v'", 58, tDto.Minutes)
+	}
+
+	if 32 != tDto.Seconds {
+		t.Errorf("Error: Expected Seconds='%v'.  Instead, Seconds='%v'", 32, tDto.Seconds)
+	}
+
+	if 515 != tDto.Milliseconds {
+		t.Errorf("Error: Expected Milliseconds='%v'.  Instead, Milliseconds='%v'", 512, tDto.Milliseconds)
+	}
+
+	if 539 != tDto.Microseconds {
+		t.Errorf("Error: Expected Microseconds='%v'.  Instead, Microseconds='%v'", 539, tDto.Microseconds)
+	}
+
+	if 300 != tDto.Nanoseconds {
+		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 300, tDto.Nanoseconds)
+	}
+
+	if 515539300 != tDto.TotNanoseconds {
+		t.Errorf("Error: Expected Total Nanoseconds='%v'.  Instead, Total Nanoseconds='%v'", 515539300, tDto.TotNanoseconds)
+	}
+
+	t2Dto := tDto.CopyOut()
+
+	if !t2Dto.Equal(tDto) {
+		t.Error("Error: Expected t2Dto to EQUAL tDto. It did NOT!")
+	}
+}
+
+func TestTimeDto_Equal_02(t *testing.T) {
+
+	tDto, err := TimeDto{}.New(2017, 4, 0, 30, 22, 58,32,0,0,515539300)
+
+	if err != nil {
+		t.Errorf("Error returned by TimeDto{}.New(2017, 4, 0, 30, 22, 58,32,0,0,515539300). Error='%v'", err.Error())
+	}
+
+	if 2017 != tDto.Years {
+		t.Errorf("Error: Expected Years='%v'.  Instead, Years='%v'", 2017, tDto.Years)
+	}
+
+	if 4 != tDto.Months {
+		t.Errorf("Error: Expected Months='%v'.  Instead, Months='%v'", 4, tDto.Months)
+	}
+
+	if 4 != tDto.Weeks {
+		t.Errorf("Error: Expected Weeks='%v'.  Instead, Weeks='%v'", 4, tDto.Weeks)
+	}
+
+	if 2 != tDto.WeekDays {
+		t.Errorf("Error: Expected WeekDays='%v'.  Instead, WeekDays='%v'", 2, tDto.WeekDays)
+	}
+
+	if 30 != tDto.DateDays {
+		t.Errorf("Error: Expected Date Days='%v'.  Instead, Date Days='%v'", 30, tDto.DateDays)
+	}
+
+	if 22 != tDto.Hours {
+		t.Errorf("Error: Expected Hours='%v'.  Instead, Hours='%v'", 22, tDto.Hours)
+	}
+
+	if 58 != tDto.Minutes {
+		t.Errorf("Error: Expected Minutes='%v'.  Instead, Minutes='%v'", 58, tDto.Minutes)
+	}
+
+	if 32 != tDto.Seconds {
+		t.Errorf("Error: Expected Seconds='%v'.  Instead, Seconds='%v'", 32, tDto.Seconds)
+	}
+
+	if 515 != tDto.Milliseconds {
+		t.Errorf("Error: Expected Milliseconds='%v'.  Instead, Milliseconds='%v'", 512, tDto.Milliseconds)
+	}
+
+	if 539 != tDto.Microseconds {
+		t.Errorf("Error: Expected Microseconds='%v'.  Instead, Microseconds='%v'", 539, tDto.Microseconds)
+	}
+
+	if 300 != tDto.Nanoseconds {
+		t.Errorf("Error: Expected Nanoseconds='%v'.  Instead, Nanoseconds='%v'", 300, tDto.Nanoseconds)
+	}
+
+	if 515539300 != tDto.TotNanoseconds {
+		t.Errorf("Error: Expected Total Nanoseconds='%v'.  Instead, Total Nanoseconds='%v'", 515539300, tDto.TotNanoseconds)
+	}
+
+
+	t2Dto := tDto.CopyOut()
+
+	tDto.Nanoseconds = 301
+
+	if t2Dto.Equal(tDto) {
+		t.Error("Error: Expected t2Dto NOT EQUAL to tDto. It IS EQUAL!")
+	}
+}
+
 
 func TestTimeDto_New_01(t *testing.T) {
 	/*
