445b99e2902136f9356c135506e75f8463745ea1
Mike Rapp <mike@paladinacs.net>
2020-05-08 15:33:25 -0500

    Added error management tests for method TimeDurationDto.SetStartEndTimes()

--- datetime/zzttimedurationdto03_test.go
+++ datetime/zzttimedurationdto03_test.go
@@ -1616,6 +1616,212 @@ func TestTimeDurationDto_SetStartEndTimes_03(t *testing.T) {
 
 }
 
+func TestTimeDurationDto_SetStartEndTimes_04(t *testing.T) {
+
+	t1str := "02/15/2014 19:54:30.000000000 -0600 CST"
+	t2str := "04/30/2017 22:58:32.000000000 -0500 CDT"
+	fmtstr := "01/02/2006 15:04:05.000000000 -0700 MST"
+
+	t1, _ := time.Parse(fmtstr, t1str)
+	t1OutStr := t1.Format(fmtstr)
+
+	t2, _ := time.Parse(fmtstr, t2str)
+	t2OutStr := t2.Format(fmtstr)
+
+	tDto := TimeDurationDto{}
+
+	err := tDto.SetStartEndTimes(
+		t1,
+		t2,
+		TDurCalcType(0).StdYearMth(),
+		TZones.US.Central(),
+		TCalcMode.LocalTimeZone(),
+		"")
+
+	if err != nil {
+		t.Errorf("Error returned by tDto.SetStartEndTimes(...). "+""+
+			"Error='%v' ", err.Error())
+	}
+
+	if t1OutStr != tDto.startDateTimeTz.GetDateTimeValue().Format(fmtstr) {
+		t.Errorf("Error: Expected DurationTriad.startDateTimeTz of %v. Instead, got %v ",
+			t1OutStr, tDto.startDateTimeTz.GetDateTimeValue().Format(fmtstr))
+	}
+
+	if t2OutStr != tDto.endDateTimeTz.GetDateTimeValue().Format(fmtstr) {
+		t.Errorf("Error: Expected DurationTriad.endDateTimeTz of %v. Instead, got %v ",
+			t1OutStr, tDto.endDateTimeTz.GetDateTimeValue().Format(fmtstr))
+	}
+
+	tOutDur := t2.Sub(t1)
+
+	if tOutDur != tDto.timeDuration {
+		t.Errorf("Error: Expected DurationTriad.timeDuration of %v. Instead, got %v",
+			tOutDur, tDto.timeDuration)
+
+	}
+
+	outStr := tDto.GetYearMthDaysTimeStr()
+
+	expected := "3-Years 2-Months 15-Days 3-Hours 4-Minutes 2-Seconds 0-Milliseconds 0-Microseconds 0-Nanoseconds"
+
+	if expected != outStr {
+		t.Errorf("Error - Expected YrMthDay: %v. Instead, got %v", expected, outStr)
+	}
+
+	outStr = tDto.GetYearsMthsWeeksTimeStr()
+
+	expected = "3-Years 2-Months 2-Weeks 1-WeekDays 3-Hours 4-Minutes 2-Seconds 0-Milliseconds 0-Microseconds 0-Nanoseconds"
+
+	if expected != outStr {
+		t.Errorf("Error - Expected YearsMthsWeeksTime Duration: %v. Instead, got %v",
+			expected, outStr)
+	}
+
+	outStr = tDto.GetDefaultDurationStr()
+
+	expected = "28082h4m2s"
+
+	if expected != outStr {
+		t.Errorf("Error - Expected Default Duration: %v. Instead, got %v",
+			expected, outStr)
+	}
+
+	outStr, err = tDto.GetCumDaysTimeStr()
+
+	if err != nil {
+		t.Errorf("Error returned by tDto.BaseTime.GetCumDaysTimeStr(). "+
+			"Error='%v'", err.Error())
+	}
+
+	expected = "1170-Days 2-Hours 4-Minutes 2-Seconds 0-Milliseconds 0-Microseconds 0-Nanoseconds"
+
+	if expected != outStr {
+		t.Errorf("Error - Expected WeekDays Duration: %v. Instead, got %v",
+			expected, outStr)
+	}
+
+	outStr, err = tDto.GetCumHoursTimeStr()
+
+	if err != nil {
+		t.Errorf("Error returned by tDto.BaseTime.GetCumHoursTimeStr(). "+
+			"Error='%v'", err.Error())
+	}
+
+	expected = "28082-Hours 4-Minutes 2-Seconds 0-Milliseconds 0-Microseconds 0-Nanoseconds"
+
+	if expected != outStr {
+		t.Errorf("Error - Expected Hours Duration: %v. Instead, got %v", expected, outStr)
+	}
+
+	outStr = tDto.GetYrMthWkDayHrMinSecNanosecsStr()
+
+	expected = "3-Years 2-Months 2-Weeks 1-WeekDays 3-Hours 4-Minutes 2-Seconds 0-Nanoseconds"
+
+	if expected != outStr {
+		t.Errorf("Error - Expected YrMthWkDayHourSecNanosec Duration: %v. Instead, got %v",
+			expected, outStr)
+	}
+
+	outStr, err = tDto.GetCumWeeksDaysTimeStr()
+
+	if err != nil {
+		t.Errorf("Error returned by tDto.BaseTime.GetCumWeeksDaysTimeStr(). "+
+			"Error='%v'", err.Error())
+	}
+
+	expected = "167-Weeks 1-WeekDays 2-Hours 4-Minutes 2-Seconds 0-Milliseconds 0-Microseconds 0-Nanoseconds"
+
+	if expected != outStr {
+		t.Errorf("Error - Expected Weeks WeekDays Duration: %v. Instead, got %v", expected, outStr)
+	}
+
+}
+
+func TestTimeDurationDto_SetStartEndTimes_05(t *testing.T) {
+
+	t1str := "02/15/2014 19:54:30.000000000 -0600 CST"
+	t2str := "04/30/2017 22:58:32.000000000 -0500 CDT"
+	fmtstr := "01/02/2006 15:04:05.000000000 -0700 MST"
+
+	t1, _ := time.Parse(fmtstr, t1str)
+
+	t2, _ := time.Parse(fmtstr, t2str)
+
+	tDto := TimeDurationDto{}
+
+	err := tDto.SetStartEndTimes(
+		t1,
+		t2,
+		TDurCalc.None(),
+		TZones.US.Central(),
+		TCalcMode.LocalTimeZone(),
+		FmtDateTimeYrMDayFmtStr)
+
+	if err == nil {
+		t.Error("Expected an error return from tDto.SetStartEndTimes(...)\n"+
+			"because input parameter 'TDurCalc.None()' is invalid!\n" +
+			"However, NO ERROR WAS RETURNED!\n")
+	}
+
+}
+
+func TestTimeDurationDto_SetStartEndTimes_06(t *testing.T) {
+
+	t1str := "02/15/2014 19:54:30.000000000 -0600 CST"
+	t2str := "04/30/2017 22:58:32.000000000 -0500 CDT"
+	fmtstr := "01/02/2006 15:04:05.000000000 -0700 MST"
+
+	t1, _ := time.Parse(fmtstr, t1str)
+
+	t2, _ := time.Parse(fmtstr, t2str)
+
+	tDto := TimeDurationDto{}
+
+	err := tDto.SetStartEndTimes(
+		t1,
+		t2,
+		TDurCalc.StdYearMth(),
+		"Invalid time zone location name",
+		TCalcMode.LocalTimeZone(),
+		FmtDateTimeYrMDayFmtStr)
+
+	if err == nil {
+		t.Error("Expected an error return from tDto.SetStartEndTimes(...)\n"+
+			"because input parameter time zone location name is invalid!\n" +
+			"However, NO ERROR WAS RETURNED!\n")
+	}
+
+}
+
+func TestTimeDurationDto_SetStartEndTimes_07(t *testing.T) {
+
+	t1str := "02/15/2014 19:54:30.000000000 -0600 CST"
+	t2str := "04/30/2017 22:58:32.000000000 -0500 CDT"
+	fmtstr := "01/02/2006 15:04:05.000000000 -0700 MST"
+
+	t1, _ := time.Parse(fmtstr, t1str)
+
+	t2, _ := time.Parse(fmtstr, t2str)
+
+	tDto := TimeDurationDto{}
+
+	err := tDto.SetStartEndTimes(
+		t1,
+		t2,
+		TDurCalc.StdYearMth(),
+		TZones.US.Central(),
+		TCalcMode.None(),
+		FmtDateTimeYrMDayFmtStr)
+
+	if err == nil {
+		t.Error("Expected an error return from tDto.SetStartEndTimes(...)\n"+
+			"because input parameter 'TCalcMode.None()' is invalid!\n" +
+			"However, NO ERROR WAS RETURNED!\n")
+	}
+
+}
+
 func TestTimeDurationDto_SetStartEndTimesTz_01(t *testing.T) {
 	t1str := "02/15/2014 19:54:30.123456789 -0600 CST"
 	fmtstr := "01/02/2006 15:04:05.000000000 -0700 MST"
